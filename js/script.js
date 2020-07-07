f6b7e25f0ad775eb9279de95826e02eb02547779
alexamanov <alexander.amanov1@mail.ru>
2020-07-07 17:22:32 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// $('.menu-btn').on('click', function(e) {
+// 	$(this).toggleClass('menu-btn-active');
+// });
+
+$('.sl').slick();
+
+
+// $('.sl').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3
+// });
