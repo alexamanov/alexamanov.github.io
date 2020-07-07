c26533fc1cea5cc719b18f1e7432593d766d460d
alexamanov <alexander.amanov1@mail.ru>
2020-07-07 23:25:03 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-// $('.menu-btn').on('click', function(e) {
-// 	$(this).toggleClass('menu-btn-active');
-// });
+$('.menu-btn').on('click', function(e) {
+	$(this).toggleClass('menu-btn-active');
+});
 
 $('.sl').slick();
 
-
-// $('.sl').slick({
-//   infinite: true,
-//   slidesToShow: 3,
-//   slidesToScroll: 3
-// });
+var $window = $(window);
+$window.on('load scroll', function() {
+    var top = $window.scrollTop();
+    var height = $window.height();
+    if (top + height >= 1920) {
+        $('header').addClass('black');
+    } else {
+    	$('header').removeClass('black');
+    }
+});
